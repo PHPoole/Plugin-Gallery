76cb16e37d89c7958857d48ea945aa85d4c2d5fe
Arnaud Ligny <arnaud@ligny.org>
2013-10-22 00:42:03 +0200

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -1,11 +1,15 @@
 <?php
 namespace PHPoole;
+use PHPoole\Utils;
 
 /**
  * PHPoole plugin Gallery
  */
 Class Gallery extends Plugin
 {
+    const RESIZE_W = 1024;
+    const RESIZE_H = 768;
+
     public function postloopLoadPages($e)
     {
         $params = $e->getParams();
@@ -14,10 +18,14 @@ Class Gallery extends Plugin
         if (isset($pageInfo['gallery']) && !empty($pageInfo['gallery'])) {
             $galleryIterator = new \FilesystemIterator($pageInfo['gallery']);
             foreach ($galleryIterator as $galleryFile) {
-                if ($galleryFile->isFile() && strtolower($galleryFile->getExtension()) == 'jpg') {
+                if ($galleryFile->isFile()
+                    && (strtolower($galleryFile->getExtension()) == 'jpg'
+                        || strtolower($galleryFile->getExtension()) == 'png'))
+                {
                     $gallery[] = array(
-                        'name'     => $galleryFile->getBasename(),
-                        'filepath' => $galleryFile->getPathname(),
+                        'name'      => Utils\slugify($galleryFile->getBasename($galleryFile->getExtension())) . '.' . $galleryFile->getExtension(),
+                        'extension' => $galleryFile->getExtension(),
+                        'filepath'  => $galleryFile->getPathname(),
                     );
                 }
             }
@@ -34,13 +42,17 @@ Class Gallery extends Plugin
         if (isset($page['gallery']) && !empty($page['gallery'])) {
             foreach ($page['gallery'] as $image) {
                 copy($image['filepath'], $phpoole->getWebsitePath() . '/' . $page['path'] . '/' . $image['name']);
-                $this->imageResize($phpoole->getWebsitePath() . '/' . $page['path'] . '/' . $image['name'], 800, 600);
+                $phpoole->addMessage(sprintf("Copy %s", $image['name']));
+                $this->imageResize($phpoole->getWebsitePath() . '/' . $page['path'] . '/' . $image['name'], self::RESIZE_W, self::RESIZE_H);
+                $phpoole->addMessage(sprintf("Resize %s", $image['name']));
             }
         }
     }
 
     private function imageResize($file, $w, $h, $crop=FALSE)
     {
+        $fileInfo = new \SplFileInfo($file);
+        $fileExt =  $fileInfo->getExtension();
         list($width, $height) = getimagesize($file);
         $r = $width / $height;
         if ($crop) {
@@ -63,10 +75,20 @@ Class Gallery extends Plugin
                 $newwidth = $w;
             }
         }
-        $src = imagecreatefromjpeg($file);
+        if ($fileExt == 'jpg') {
+            $src = imagecreatefromjpeg($file);
+        }
+        elseif ($fileExt == 'png') {
+            $src = imagecreatefrompng($file);
+        }
         $dst = imagecreatetruecolor($newwidth, $newheight);
         imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
         imagedestroy($src);
-        imagejpeg($dst, $file, 100);
+        if ($fileExt == 'jpg') {
+            imagejpeg($dst, $file, 100);
+        }
+        elseif ($fileExt == 'png') {
+            imagepng($dst, $file, 0);
+        }
     }
 }
